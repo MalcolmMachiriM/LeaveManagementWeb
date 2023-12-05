6162bf497fe14afeda780e1dba2ee5b87e034b80
Malcolm <machirimalcolm@gmail.com>
2023-12-05 16:22:53 +0200

    --added an intereface

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<bool> Exists(int id);
+        Task<T> DeleteAsync(int id);
+        Task<T> UpdateAsync(T entity);
+
+    }
+}
