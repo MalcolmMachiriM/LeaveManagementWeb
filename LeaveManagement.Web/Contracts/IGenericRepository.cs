3cc6da9b448365320e761314551f355806983710
Malcolm <machirimalcolm@gmail.com>
2023-12-05 17:32:58 +0200

    -- used contracts to abstract a lot of stuff using generics and all

--- LeaveManagement.Web/Contracts/IGenericRepository.cs
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -2,12 +2,12 @@
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetAsync(int id);
+        Task<T> GetAsync(int? id);
         Task<List<T>> GetAllAsync();
         Task<T> AddAsync(T entity);
         Task<bool> Exists(int id);
-        Task<T> DeleteAsync(int id);
-        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
 
     }
 }
