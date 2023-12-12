2e784c59d74a08a9504cd153ecf06f1603d7ff8c
Malcolm <machirimalcolm@gmail.com>
2023-12-12 16:30:14 +0200

    -- new contract for allocation

---
+++ LeaveManagement.Web/Contracts/IleaveAllocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Contracts
+{
+    public interface IleaveAllocationRepository : IGenericRepository<LeaveAllocation>
+    {
+        Task LeaveAllocation(int leaveTypeId);
+    }
+}
