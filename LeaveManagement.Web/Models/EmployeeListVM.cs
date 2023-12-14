b4e2d9a22b3414dd00a1cb758e42c417d48c723f
Malcolm <machirimalcolm@gmail.com>
2023-12-14 13:02:52 +0200

    -- created new vm

---
+++ LeaveManagement.Web/Models/EmployeeListVM.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement.Web.Models
+{
+    public class EmployeeListVM
+    {
+        public string Id { get; set; }
+        [D]
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string DateJoined { get; set; }
+    }
+}
