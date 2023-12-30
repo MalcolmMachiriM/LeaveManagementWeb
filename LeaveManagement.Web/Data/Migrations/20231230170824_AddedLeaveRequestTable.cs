67c0315a4163ccf6f47e236af02d8f93ee351b0c
Malcolm <machirimalcolm@gmail.com>
2023-12-30 19:56:03 +0200

    --leave request repository and all

---
+++ LeaveManagement.Web/Data/Migrations/20231230170824_AddedLeaveRequestTable.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class AddedLeaveRequestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeaveRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    DateRequested = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequestComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Approved = table.Column<bool>(type: "bit", nullable: true),
+                    Cancelled = table.Column<bool>(type: "bit", nullable: false),
+                    RequestingEmployeeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_LeaveTypes_LeaveTypeId",
+                        column: x => x.LeaveTypeId,
+                        principalTable: "LeaveTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-1234-7896-9587-283f579e4764",
+                column: "ConcurrencyStamp",
+                value: "25768bd6-3b87-4e64-8c1b-2259b0f57cb9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-1728-44eb-1995-283f579e4764",
+                column: "ConcurrencyStamp",
+                value: "31ac4082-ae24-4f91-a03f-d3d8e266f7f1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-4238-44eb-9587-283f579e4764",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "bbd76c5a-3e5b-41e9-aeb8-d3673aca0ee2", "AQAAAAEAACcQAAAAEO/q2jV/p4Emu6nTuZ9xcRZfOOr33AJv7RaV9Pbob0KUucf7/RwGu2OB+UCqo3rVBg==", "3f94a156-da1f-499c-a311-45e427419b4f" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e18dc662-c956-45fc-a834-63128024ce27",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "33afc127-a278-49fd-b158-de44ee1aee28", "AQAAAAEAACcQAAAAECfjaOI5bTj/nhHXYICbuP2mrj8Z9oUjy2eX0fFsgKj8A4X+VuClhm34MDLbOALutQ==", "df4dca89-f6ad-4831-a9b3-3d28baa28793" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_LeaveTypeId",
+                table: "LeaveRequests",
+                column: "LeaveTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveRequests");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-1234-7896-9587-283f579e4764",
+                column: "ConcurrencyStamp",
+                value: "37543a59-25f7-4c49-9201-a50852aa2f44");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-1728-44eb-1995-283f579e4764",
+                column: "ConcurrencyStamp",
+                value: "4417d91c-3ec1-46a5-b7c7-c0f0c654e38b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "73ad90b0-4238-44eb-9587-283f579e4764",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "592a89b8-8055-458d-918c-70634cf94b41", "AQAAAAEAACcQAAAAEB+/19aZgh5YYl5DY66bXHUv+KOj/xJBVGxdsBkfmBi59GTbThl1HcoTjvrTarlzgA==", "5c3c435e-d33a-4aaa-9140-40ec6a2d4a73" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e18dc662-c956-45fc-a834-63128024ce27",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d905cd78-1b83-40dd-b6a6-9fe6fac0f6e1", "AQAAAAEAACcQAAAAEBIq3ZAKRCTCvJeOux1VpfcZ68UVVF5ha3TA77bMjpdkBNOdn5Ur55D9jJ7IgfbE3A==", "fe230432-9ba1-495a-a008-88822dc3d9f5" });
+        }
+    }
+}
--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddTransient<IEmailSender>(s => new EmailSenderr("localhost", 2
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
 builder.Services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+builder.Services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
